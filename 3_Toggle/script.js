0fcc92b3274414da3023cb6ab50e98dc46eebb12
juanomanino <juannino@colegioguanenta.edu.co>
2023-05-16 09:59:36 -0500

    P3-Explicaciones

--- 3_Toggle/script.js
+++ 3_Toggle/script.js
@@ -1,6 +1,8 @@
+//Encontramos las clases de los elementos HTML y las guardamos en variables
 let toggleBtn=document.querySelector('.toggleBtn')
 let container=document.querySelector('.container')
 
+//Cuando se clicka el toggleBtn, aplica la funciión que en el container, lista las clases y reconoce quien se comporta como toggle(al click se activa, al otro click se apaga) cuando se activa la clase active.
 toggleBtn.onclick=function(){
     container.classList.toggle('active')
 
